c19455633ca3c447aa3ae44cb296475b6a4f93ef
JoseEscobar23 <karinaescobar629@gmail.com>
2025-02-25 22:14:54 -0400

    Primer commit (ningun cambio)

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //ejercicios simples mostrando en pantalla
+        //tipos de datos
+        byte edad = 21;
+        int numerogrande= 2145;
+        float tamaño= 60.1f;
+        double vida=54.4;
+        System.out.println("Edad"+ edad);
+        System.out.println("Numero Grande"+numerogrande);
+        System.out.println("Tamaño"+tamaño);
+        System.out.println("Vida"+vida);
+        //si es verdadero o falso
+        boolean activo =true;
+        //guardar solo un dato
+        char curso = 'A';
+        System.out.println("estado"+activo);
+        System.out.println("curso"+curso);
+    }
+}
